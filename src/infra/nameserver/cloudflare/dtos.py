500feb417c8a636738522175c1ef3a81d99b55fc
anthonyafgx <89319333+anthonyafgx@users.noreply.github.com>
2024-10-27 19:15:12 -0400

    Cloudflare DTOs

--- src/infra/nameserver/cloudflare/dtos.py
+++ src/infra/nameserver/cloudflare/dtos.py
@@ -1,17 +1,33 @@
 from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional, Dict
+from ipaddress import IPv4Address
 
-class CloudflareDNSRecord(BaseModel):
+class CloudflareDNSRecordDTO(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
     id: str
-    type: str
     name: str
-    content: str
-    proxiable: bool
-    proxied: bool
-    ttl: int
-    locked: bool
-    zone_id: str
-    zone_name: str
-    created_on: str
-    modified_on: str
-    data: Optional[dict] = Field(default_factory=dict)
+    content: IPv4Address
+
+class CloudflareMessageDTO(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    code: int = Field(..., gt=0, le=1000)
+    message: str
+
+class CloudflareResultInfoDTO(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    count: int = Field(..., ge=0)
+    page: int = Field(..., ge=0)
+    per_page: int = Field(..., ge=0)
+    total_count: int = Field(..., ge=0) 
+
+class CloudflareListDNSRecordsDTO(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    success: bool
+    errors: list[CloudflareMessageDTO] = Field(default_factory=list)
+    messages: list[CloudflareMessageDTO] = Field(default_factory=list)
+    result_info: CloudflareResultInfoDTO
+    result: list[CloudflareDNSRecordDTO]
